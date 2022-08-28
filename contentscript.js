addc76fb0bd1ce3fb87c70eb933fad09ad723bf0
luca2618 <lucassylvester02@gmail.com>
2022-08-28 02:22:57 +0200

    it works

--- contentscript.js
+++ contentscript.js
@@ -4,14 +4,17 @@ debugger;
 
 data = document.getElementsByClassName("overlayArea");
 filtered = data.item(0).children.item(0).children
-
 course_spots = []
 course_names = []
 
 for (let i = 1; i < filtered.length; i++) {
     spot = filtered.item(i).children.item(0).children.item(2).innerText
     name = filtered.item(i).children.item(0).children.item(1).innerText
-
+    if (spot.includes("7")){
+        spot = "7";
+    } else {
+        spot = spot.slice(1,3);
+    }
     course_spots.push(spot)
     course_names.push(name)
   }
--- popup.js
+++ popup.js
@@ -1,13 +1,46 @@
 debugger;
 
-//course_spots = chrome.storage.local.get(['course_spots']).result;
-//course_names = chrome.storage.local.get(['course_names']).result;
+//course_spots = chrome.storage.local.get(['course_spots']).then();
+//course_names = chrome.storage.local.get(['course_names']);
 
-course_spots = ["1A"];
-course_names = ["trippelbong"]
+//console.log(course_spots)
+//doesent look nice but whatever
+/*
+all_spots = ["1A","2A","3A","4A","5A","1B","2B","3B","4B","5B","7"]
+for (let i = 0; i < all_spots.length; i++) {
+    element = document.getElementById(all_spots[i])
+    element.textContent = "";
+}
+*/
 
+chrome.storage.local.get(['course_names']).then(
+    function(value) {
+        course_names = value['course_names'];
+        chrome.storage.local.get(['course_spots']).then(
+            function(value) {
+                course_spots = value['course_spots'];
+                console.log(course_spots);
+                for (let i = 0; i < course_spots.length; i++) {
+                    element = document.getElementById(course_spots[i]);
+                    console.log(element)
+                    element.textContent = course_names[i]
+                    }
+        
+            },
+            function(error) {myDisplayer(error);}
+          );
+    },
+    function(error) {myDisplayer(error);}
+  );
+
+
+
+
+//course_spots = ["1A"];
+//course_names = ["trippelbong"]
+/*
 for (let i = 0; i < course_spots.length; i++) {
 element = document.getElementById(course_spots[i]);
 console.log(element)
 element.textContent = course_names[i]
-}
+}*/
