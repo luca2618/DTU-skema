20429a35e63e4edce7ee663f13b0e04b53204ebd
luca2618 <lucassylvester02@gmail.com>
2022-08-30 14:23:16 +0200

    Link on courses

--- contentscript.js
+++ contentscript.js
@@ -1,13 +1,12 @@
 debugger;
 
+function update(){
+    data = document.getElementsByClassName("overlayArea");
+    filtered = data.item(0).children.item(0).children
+    course_spots = []
+    course_names = []
 
-
-data = document.getElementsByClassName("overlayArea");
-filtered = data.item(0).children.item(0).children
-course_spots = []
-course_names = []
-
-for (let i = 1; i < filtered.length; i++) {
+    for (let i = 1; i < filtered.length; i++) {
     spot = filtered.item(i).children.item(0).children.item(2).innerText
     name = filtered.item(i).children.item(0).children.item(1).innerText
     if (spot.includes("7")){
@@ -17,11 +16,14 @@ for (let i = 1; i < filtered.length; i++) {
     }
     course_spots.push(spot)
     course_names.push(name)
-  }
-  console.log(course_spots)
-  console.log(course_names)
+    }
+    console.log(course_spots)
+    console.log(course_names)
 
-chrome.storage.local.set({course_spots: course_spots})
-chrome.storage.local.set({course_names: course_names})
- 
+    chrome.storage.local.set({course_spots: course_spots})
+    chrome.storage.local.set({course_names: course_names})
+}
+update()
+
+
 
--- popup.js
+++ popup.js
@@ -1,16 +1,29 @@
 debugger;
 
+
+
 //course_spots = chrome.storage.local.get(['course_spots']).then();
 //course_names = chrome.storage.local.get(['course_names']);
 
 //console.log(course_spots)
 //doesent look nice but whatever
 
-all_spots = ["1A","2A","3A","4A","5A","1B","2B","3B","4B","5B","7"]
+chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    url = tabs[0].url;
+    // use `url` here inside the callback because it's asynchronous!
+    if (url.includes("studieplan.dtu.dk")){
+    a = 1
+    }
+});
+
+
+
+
+/*all_spots = ["1A","2A","3A","4A","5A","1B","2B","3B","4B","5B","7"]
 for (let i = 0; i < all_spots.length; i++) {
     element = document.getElementById(all_spots[i])
     element.textContent = "";
-}
+}*/
 
 
 chrome.storage.local.get(['course_names']).then(
@@ -23,7 +36,7 @@ chrome.storage.local.get(['course_names']).then(
                 for (let i = 0; i < course_spots.length; i++) {
                     element = document.getElementById(course_spots[i]);
                     console.log(element)
-                    element.textContent = course_names[i]
+                    element.innerHTML = "<a href="+"https://kurser.dtu.dk/course/"+course_names[i].slice(0,5)+ " target=\"_blank\">"+course_names[i]+"</a>"
                     }
         
             },
